2b84386cfbe35f1ea30e6408772e1955cde3f515
crdx <pub@crdx.org>
2025-04-05 23:25:04 +0100

    Suppress Naming/PredicateName rubocop lint

--- lib/dotman/actions/link.rb
+++ lib/dotman/actions/link.rb
@@ -22,7 +22,7 @@ module Dotman::Action
             Shell.statement(link_is_set_up, link_set_up, link_not_set_up)
         end
 
-        def does_not_exist
+        def does_not_exist # rubocop:disable Naming/PredicateName
             Shell.comparison('$HOME_DIR/' + to, '! -e')
         end
 
